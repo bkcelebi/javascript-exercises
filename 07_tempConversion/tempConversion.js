a0f8e8db04faa12aa82f2fc27abced6547905b70
KC_Dev <burakkcelebi@hotmail.com>
2023-01-15 11:23:49 +0000

    Practiced! Testing with Jest is practiced.
    The samples sumAll, leapYears and tempConversion are done by testing and meeting with the required result by using Jest.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+
+  let result = (fah - 32) * 5/9;
+  // console.log(result);
+  let oneDec = Math.round(result * 10) / 10;
+  console.log(oneDec);
+  return oneDec;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cal) {
+
+  let result = (cal * 9/5) + 32;
+  // console.log(result);
+  let oneDec = Math.round(result * 10) / 10;
+  console.log(oneDec);
+  return oneDec;
+
 };
 
 // Do not edit below this line
