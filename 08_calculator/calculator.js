e2b835803f7fe340f7c83650c0525782592a7203
KC_Dev <burakkcelebi@hotmail.com>
2023-02-07 23:09:03 +0000

    practiced, practiced with calculator sample
    add, subtract, sum, multiply, power and factorial functions are practiced

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(para1, para2) {
+  return para1 + para2;
 };
 
-const subtract = function() {
-	
+const subtract = function(para1, para2) {
+	return para1 - para2;
 };
 
-const sum = function() {
-	
+const sum = function(para) {
+  let sum = 0;
+  for (let i of para) sum += i;
+  return Number(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(para) {
+  let sum = 1;
+  for (let i of para) sum *= i;
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(para1, para2) {
+	return Math.pow(para1, para2);
 };
 
-const factorial = function() {
+const factorial = function(para) {
 	
+  if (para < 0) return -1;
+  else if (para === 0) return 1;
+  else return para * factorial(para - 1);
+  
 };
 
 // Do not edit below this line
